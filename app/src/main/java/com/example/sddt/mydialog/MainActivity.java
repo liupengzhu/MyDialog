fae28c11e8c76423407cb0573dfad3f91e9e1168
朱留鹏 <>
2018-01-17 17:15:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sddt/mydialog/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.sddt.mydialog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    private SelfDialog selfDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selfDialog = new SelfDialog(MainActivity.this);
+                selfDialog.setTitle("提示");
+                selfDialog.setMessage("确定退出应用?");
+                selfDialog.setYesOnclickListener("确定", new SelfDialog.onYesOnclickListener() {
+                    @Override
+                    public void onYesClick() {
+                        Toast.makeText(MainActivity.this,"点击了--确定--按钮",Toast.LENGTH_LONG).show();
+                        selfDialog.dismiss();
+                    }
+                });
+                selfDialog.setNoOnclickListener("取消", new SelfDialog.onNoOnclickListener() {
+                    @Override
+                    public void onNoClick() {
+                        Toast.makeText(MainActivity.this,"点击了--取消--按钮",Toast.LENGTH_LONG).show();
+                        selfDialog.dismiss();
+                    }
+                });
+                selfDialog.show();
+            }
+        });
+    }
+}
